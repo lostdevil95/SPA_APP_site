b9dd41f109e451dc64e245df5309770bd9d4a9e6
lostdevil95 <g-unit_m@mail.ru>
2022-01-23 15:25:59 +0300

    new brand logo and other fix in post_detail page

--- spa_blog/spa_blog/settings.py
+++ spa_blog/spa_blog/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    'spablog/static',
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CKEDITOR_UPLOAD_PATH = "uploads/"
